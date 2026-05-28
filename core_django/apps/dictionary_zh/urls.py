e0a44fb76dbe43652a7114f60774428590dfc6eb
hoahung2307 <angelnuyn@gmail.com>
2026-05-28 21:08:01 +0700

    Add Chinese translation task and dedicated Celery queues

--- core_django/apps/dictionary_zh/urls.py
+++ core_django/apps/dictionary_zh/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ZhWordSearchView
+from .views import ZhWordSearchView, PureTextTranslationView, TranslationStatusView
 
 urlpatterns = [
     path('search/', ZhWordSearchView.as_view(), name='zh_word_search'),
+    path('translate/', PureTextTranslationView.as_view(), name='zh_translate'),
+    path('translate/status/<str:task_id>/', TranslationStatusView.as_view(), name='zh_translate_status'),
 ]
